9cf3ebf227013657ea915f6c9c12bc81d6a18cee
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-06-03 15:07:25 -0700

    pseudo code moved from readme to JS file

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,5 +1,66 @@
 // create multiple divs to display and not display
 // upon click show next div and start timer
+
+// pseudo code:
+//front end
+
+//div for title id="TopTitle"
+
+//div start btn  id="start"
+
+//div store all question and ans and timer
+
+//div for timer id="timer"
+
+//div for question id="question"
+
+//div for answer id="answer"
+
+
+
+//backend
+//timer .. class
+
+//stopTimer fx
+
+//restart timer fx
+
+//timer fx that decrements the time
+
+// display div 
+
+
+//nextQuestion() function
+//display timer
+//display one question at a time 
+//display answers
+ 
+
+
+
+//if users clicks
+//check to see if it not the end of the game (array length) 
+	//if correct
+	//new screen says congrats
+	//then go to next question nextQuestion()
+	//else
+	//stop timer
+	//reset timer
+	//this means users is incorrect
+	//tell user wrong
+	//display correct ans
+	//stop timer
+	//reset timer
+	//go to next question nextQuestion()
+
+//else
+//you are at the end of the game
+//display final screen show correct ans.
+// incorr ans
+//option to restart game
+
+
+
 var startTimer;
 var number = 30;
 var intervalId;
